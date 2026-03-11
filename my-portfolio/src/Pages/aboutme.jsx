7d828ed4aa88e429d9bfaf4d77fc606e5c95384f
Abhishekgorshi <abhishekgorshi76@gmail.com>
2026-03-11 08:59:32 +0530

    update resume

--- my-portfolio/src/Pages/aboutme.jsx
+++ my-portfolio/src/Pages/aboutme.jsx
@@ -23,10 +23,10 @@ function AboutMe() {
               I enjoy solving problems and learning new technologies.
             </p>
             <p>Btech CSE student with strong knowledge of MERN stack.
-Built 3+ full-stack projects.
-Interested in backend & problem solving.
-Currently improving DSA & system design.
-</p>
+             Built 3+ full-stack projects.
+             Interested in backend & problem solving.
+             Currently improving DSA & system design.
+             </p>
 
             <div className="about-info">
               <p><strong>Email:</strong> abhishekgorshi76@gmail.com </p>
